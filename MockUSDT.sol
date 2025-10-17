ffacd82469ca8ec259eaad4a0e168898299a962f
Gabriel Natter <nattergabriel@gmail.com>
2025-10-17 02:07:45 +0200

    feat: add descripton for mocked USDC

--- MockUSDT.sol
+++ MockUSDT.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title MockUSDC
+ * @notice This is a mock version of USDC, created for local testing and development only.
+ * @dev Do NOT use this contract in production or on mainnet.
+ * It mints test tokens to the deployer so you can simulate USDC behavior in Remix or testnets.
+ */
 contract MockUSDC is ERC20 {
     constructor() ERC20("USD Coin", "USDC") {
         _mint(msg.sender, 1_000_000 * 10 ** 6);
